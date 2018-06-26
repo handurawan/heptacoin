801f2e826233c973ecf451d1cd9951e16cfbb955
Heptacoin <heptacoindev@gmail.com>
2018-06-26 15:25:55 +0200

    Update pszTimestamp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -42,16 +42,13 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  * transaction cannot be spent since it did not originally exist in the
  * database.
  *
- * CBlock(hash=000000000019d6, ver=1, hashPrevBlock=00000000000000, hashMerkleRoot=4a5e1e, nTime=1231006505, nBits=1d00ffff, nNonce=2083236893, vtx=1)
- *   CTransaction(hash=4a5e1e, ver=1, vin.size=1, vout.size=1, nLockTime=0)
- *     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73)
- *     CTxOut(nValue=50.00000000, scriptPubKey=0x5F1DF16B2B704C8A578D0B)
- *   vMerkleTree: 4a5e1e
+ * >>> from pyblake2 import blake2s
+ * >>> 'Heptacoin' + blake2s(b'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks BTC#433272 000000000000000002c46a68972be8e6f671774e773449ea81c198243e3782c8 LTC#741825 27659c79fbb898e9c35f55d49126afcc3a63d1501d399651f21980c538cbaa8f DJIA close on 29 Nov 2017: 23,940.68').hexdigest()
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
-    const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
+    const char* pszTimestamp = "Heptacoinab3e86bd60ba1159b47e82bd176f4b777d3ab1659ab8cbf0087a9f57c341815e";
+    const CScript genesisOutputScript = CScript() << ParseHex("04f198550e8e8de399be101a606ebe23daf504439e359657c57256c424df0fe20948cfbac6528ebbe595d39062381f556def502e14eba169e7d7828d7fb843e0de") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -183,7 +183,7 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
 {
     // Note that by default, these tests run with size accounting enabled.
     const CChainParams& chainparams = Params(CBaseChainParams::MAIN);
-    CScript scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
+    CScript scriptPubKey = CScript() << ParseHex("04f198550e8e8de399be101a606ebe23daf504439e359657c57256c424df0fe20948cfbac6528ebbe595d39062381f556def502e14eba169e7d7828d7fb843e0de") << OP_CHECKSIG;
     CBlockTemplate *pblocktemplate;
     CMutableTransaction tx,tx2;
     CScript script;
