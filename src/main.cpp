59b025a7eb5e3127c690aafdbc20032ba4124dda
Heptacoin <heptacoindev@gmail.com>
2018-07-15 03:02:54 +0200

    CheckEquihashSolution in ReadBlockFromDisk

--- src/main.cpp
+++ src/main.cpp
@@ -1710,7 +1710,8 @@ bool ReadBlockFromDisk(CBlock& block, const CDiskBlockPos& pos, const Consensus:
     }
 
     // Check the header
-    if (!CheckProofOfWork(block.GetHash(), block.nBits, consensusParams))
+    if (!(CheckEquihashSolution(&block, Params()) &&
+          CheckProofOfWork(block.GetHash(), block.nBits, consensusParams)))
         return error("ReadBlockFromDisk: Errors in block header at %s", pos.ToString());
 
     return true;
